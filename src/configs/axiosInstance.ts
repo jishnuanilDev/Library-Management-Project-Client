fc45530a1860c604f3d462784029e143bbcd83a3
jishnu anil <jishnuanil255@gmail.com>
2025-02-07 18:16:39 +0530

    undo apiUrl changes

--- src/configs/axiosInstance.ts
+++ src/configs/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
-  baseURL: 'https://library-management-server-livid.vercel.app',
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
   },
