1dfcdb8874d87453efeded091692bdea6216a96f
jishnu anil <jishnuanil255@gmail.com>
2025-02-07 16:38:23 +0530

    new fixed made

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // If you were using any tailwind or flowbite-related configurations, remove them
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", 
+  },
 });
